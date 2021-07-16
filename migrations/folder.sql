93ea33bf9a6bf068c908525dfae44cf03b5105f7
Sheila Aman <sheila@vulpine.house>
2021-07-15 21:23:50 -0500

    folder.sql: add blurb field

--- migrations/folder.sql
+++ migrations/folder.sql
@@ -3,6 +3,9 @@ create table if not exists folders (
     folder_id serial primary key,
     user_id foreign key (users.user_id) not null,
     title varchar(127) not null,
+    blurb text not null,
+    created date not null,
+    modified date not null,
     about text
 );
 
