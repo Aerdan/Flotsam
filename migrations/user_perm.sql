fc70d478bf4817687607755ac2873287d0eb1e3a
Sheila Aman <sheila@vulpine.house>
2021-07-16 08:54:25 -0500

    user_perm: more permissions

--- migrations/user_perm.sql
+++ migrations/user_perm.sql
@@ -4,10 +4,18 @@ create table if not exists user_permissions (
     user_id foreign key(users.user_id),
     can_view_site boolean default true,
     can_view_users boolean default false,
-    can_add boolean default false,
-    can_modify boolean default false,
-    can_email_ok boolean default false,
-    can_mfa_reset boolean default false
+    can_add_user boolean default false,
+    can_modify_user boolean default false,
+    can_verify_user boolean default false,
+    can_reset_user_mfa boolean default false,
+    can_delete_user boolean default false,
+    can_delete_post boolean default false,
+    can_delete_folder boolean default false,
+    can_delete_contact boolean default false,
+    can_disable_user boolean default false,
+    can_disable_post boolean default false,
+    can_disable_folder boolean default false,
+    can_disable_contact boolean default false
 );
 
 -- 1 down
